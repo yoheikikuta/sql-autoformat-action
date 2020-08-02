2c4dc4868cdfef07799d5a47954d6a40a673e2f5
Yohei Kikuta <diracdiego@gmail.com>
2020-08-02 20:34:49 +0900

    Unformat queries to check autoformatter (#1)
    * Unformat queries to check autoformatter
    
    * [Fix] use context to distinguish PR from push
    
    Use context to push commits appropriately in pull request events.
    Add some files to do that.
    
    * [TEST] Auto-format queries
    
    Co-authored-by: [TEST] BigQuery AutoFormatter <test+test@example.com>

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+// https://github.com/samuelmeuli/lint-action/blob/master/LICENSE.md
+
 const git = require('./git');
+const { getContext } = require("./github/context");
 const { getInput, log, run } = require("./utils/action");
 
 function runAction() {
     log('Get inputs.')
+    const context = getContext();
     const autoFix = getInput("auto_fix") === "true";
     const gitName = getInput("git_name", true);
     const gitEmail = getInput("git_email", true);
@@ -14,6 +18,19 @@ function runAction() {
         output = run('./src/format_all_sql_files.sh');
     } catch (ex) {
         output = ex.stdout;
+    }
+
+	if (context.eventName === "pull_request") {
+		// Fetch and check out PR branch:
+		// - "push" event: Already on correct branch
+		// - "pull_request" event on origin, for code on origin: The Checkout Action
+		//   (https://github.com/actions/checkout) checks out the PR's test merge commit instead of the
+		//   PR branch. Git is therefore in detached head state. To be able to push changes, the branch
+		//   needs to be fetched and checked out first
+		// - "pull_request" event on origin, for code on fork: Same as above, but the repo/branch where
+		//   changes need to be pushed is not yet available. The fork needs to be added as a Git remote
+		//   first
+		git.checkOutRemoteBranch(context);
     }
 
     log('If the queries are formatted, push them.');
